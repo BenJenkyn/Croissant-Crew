6932d52d81cbd12fedcb0e536afa959b68d42f65
BenJenkyn <jenkynb@gmail.com>
2025-05-09 17:46:13 -0400

    adds bullet

---
+++ Assets/Bullet.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Bullet hit: " + collision.gameObject.name);
+    }
+}
